3b69642ab7d82d33ae7a410ecc09bdae0d26dd2f
yxjsxy <34668252+yxjsxy@users.noreply.github.com>
2026-07-03 21:01:48 -0700

    chore: refresh WorldCup after match

--- scripts/update_schedule.mjs
+++ scripts/update_schedule.mjs
@@ -87,6 +87,7 @@ const playerNameZh = new Map([
   ["Cyle Larin", "塞尔·拉林"],
   ["Stephen Eustaquio", "斯蒂芬·欧斯塔基奥"],
   ["Damián Bobadilla", "达米安·博瓦迪利亚"],
+  ["Jhon Arias", "约翰·阿里亚斯"],
   ["Folarin Balogun", "弗拉林·巴洛贡"],
   ["Malik Tillman", "马利克·蒂尔曼"],
   ["Auston Trusty", "奥斯顿·特拉斯蒂"],
